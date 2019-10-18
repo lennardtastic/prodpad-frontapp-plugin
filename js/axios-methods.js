ce198dfcc53f93db6a8874f2bd08261a97c5cd87
Lennard <lennard@usabilla.com>
2019-10-18 15:07:35 +0200

    no message

--- js/axios-methods.js
+++ js/axios-methods.js
@@ -38,6 +38,9 @@ var prodpadPostIdeaPayload = {
   }],
 };
 
+// Variables Frontapp
+var conversation;
+
 /**
  * Function for updating the payload variables
  */
@@ -64,10 +67,10 @@ function prepareProdpadPayload() {
   prodpadPostIdeaPayload.external_links = prodpadExternalLinks;
 }
 
-Front.on('panel_visible', function (visible) {
+front.on('panel_visible', function (visible) {
   // triggered when your plugin becomes visible or invisible
   // you can also test Front.visible
-  Front.on('conversation', function (data) {
+  front.on('conversation', function (data) {
     // triggered when a conversation is loaded
     document.getElementById("inputLinkName").value = conversation.id;
     document.getElementById("inputLinkUrl").value = conversation.link;
